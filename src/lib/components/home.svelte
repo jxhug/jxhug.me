12f3329ae05fcd885e62b6e5535dd7d5bff96420
James Hughes <github@jxhug.me>
2023-07-06 14:13:21 +0100

    made sm: the overlay gradient on home

--- src/lib/components/home.svelte
+++ src/lib/components/home.svelte
@@ -1,6 +1,6 @@
 <div class="relative overflow-clip h-screen">
     <div class="bg-lightdesktopjames bg-cover h-screen dark:bg-darkdesktopjames snap-center"></div>
-    <div class="absolute inset-0 bg-gradient-to-t from-darkwhite to-transparent pointer-events-none dark:from-darkergray snap-center">
+    <div class="absolute inset-0 sm:bg-gradient-to-t sm:from-darkwhite sm:to-transparent pointer-events-none sm:dark:from-darkergray snap-center">
         <div class="flex flex-col justify-center h-screen snap-center">
             <div class="font-extrabold text-slate-600 text-7xl sm:text-7xl lg:text-8xl 2xl:text-9xl dark:text-darkwhite justify-center text-left w-min md:w-screen md:translate-x-3/20 mx-auto md:mx-0"> <!-- this one-->
                 <h1 class="">

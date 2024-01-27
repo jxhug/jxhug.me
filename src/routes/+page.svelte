23b89eba5c403e9324dea97fec2c41fc0eb0c433
James Hughes <james@jxhug.me>
2024-01-26 21:39:13 -0800

    Update phoenix URL

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -121,7 +121,7 @@
     <div bind:this={projectSection} class="space-y-32 md:space-y-20 lg:space-y-0">
         <div class="h-screen">
             <div class="w-[90vw] h-1/2">
-                <Project link="https://phoenixlauncher.vercel.app" name="Phoenix" description="A lightweight and open source game launcher for macOS." tools="Built with Swift and SwiftUI." imageName="phoenix" flipped={true} mobile={false} />
+                <Project link="https://phoenixlauncher.app" name="Phoenix" description="A lightweight and open source game launcher for macOS." tools="Built with Swift and SwiftUI." imageName="phoenix" flipped={true} mobile={false} />
             </div> 
             <div class="w-[90vw] h-1/2">
                 <Project link="https://www.spigotmc.org/resources/biomerace.96241/" name="BiomeRace" description="A Minecraft plugin where players race to see who can find a random biome first!" tools="Built with Java and Spigot." imageName="biomerace" flipped={false} mobile={false}/>
